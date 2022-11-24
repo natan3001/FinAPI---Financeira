47f88ba3986cb838fb6cc2e929ef7bc993aec32d
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-24 15:21:23 -0300

    Adicionado função para cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-const express = require('express');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({
-    message: 'Hello World!'
-  })
-})
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    name,
+    cpf,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
